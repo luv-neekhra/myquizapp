22358e2d59b2065858f5c1beedb2ed9cbdc4c6c8
luv-neekhra <neekhraluv@gmail.com>
2024-06-01 15:14:46 +0530

    My Quiz

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is the first calculating device?",
+    answer: "Abacus",
+    options: [
+      "Abacus",
+      "Calculator",
+      "Turing Machine",
+      "Pascaline"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who among the following considered as the ‘father of artificial intelligence’?",
+    answer: "John McCarthy",
+    options: [
+      "Charles Babbage",
+      "Lee De Forest",
+      "John McCarthy",
+      "JP Eckert"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who among the following Cricketer was the winner of Orange Cap in the Indian Premier League (IPL) 2024?",
+    answer: "Virat Kohli",
+    options: [
+      "Virat Kohli",
+      "Rohit Sharma",
+      "K L Rahul",
+      "Kieron Pollard"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many players are there in a team of Volleyball?",
+    answer: "6",
+    options: [
+      "5",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+];
